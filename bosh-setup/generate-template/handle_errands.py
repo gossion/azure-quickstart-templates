8c8449b0187e1b7277975b1598e9d02ea18de1aa
guwe <guwe@microsoft.com>
2016-11-21 01:59:33 +0000

    Upgrade cf-release to v244

---
+++ bosh-setup/generate-template/handle_errands.py
@@ -0,0 +1,45 @@
+from ruamel import yaml
+import sys
+
+# update cf_diego_template
+# 1. inject cf_template.properties.etcd of azure-cf.yml to cf_diego_template.jobs.etcd_zX.properties
+# 2. set cf_diego_template.properties.etcd to {}
+
+cf_template       = sys.argv[1]
+diego_template    = sys.argv[2]
+cf_diego_template = sys.argv[3]
+
+with open(cf_template, 'r') as stream:
+  cf = yaml.load(stream)
+with open(diego_template, 'r') as stream:
+  diego = yaml.load(stream)
+with open(cf_diego_template, 'r') as stream:
+  cf_diego = yaml.load(stream)
+
+cf_etcd_properties = cf['properties']['etcd']
+diego_etcd_properties = diego['properties']['etcd']
+
+for job in cf_diego['jobs']:
+  if job['name'].startswith('etcd_z'):
+    job['properties']['etcd'] = cf_etcd_properties
+cf_diego['properties']['etcd'] = {}
+
+# update cf_diego_template
+# Comment below changes because the manifest has generated many static IPs which affect not only job.networks.static_ip but also many properties.
+#
+#cf_diego_networks = cf_diego['networks']
+#for network in cf_diego_networks:
+#  if network['type'] == 'vip':
+#    continue
+#  network['subnets'][0]['dns'] = ["REPLACE_WITH_DNS"]
+#  network['subnets'][0]['gateway'] = ["REPLACE_WITH_GATEWAY_IP"]
+#  if network['name'].startswith('cf'):
+#    network['subnets'][0]['reserved'] = ["REPLACE_WITH_CF_RESERVED_IP_FROM - REPLACE_WITH_CF_RESERVED_IP_TO"]
+#    network['subnets'][0]['static'] = ["REPLACE_WITH_CF_STATIC_IP_FROM - REPLACE_WITH_CF_STATIC_IP_TO"]
+#  if network['name'].startswith('diego'):
+#    network['subnets'][0]['reserved'] = ["REPLACE_WITH_DIEGO_RESERVED_IP_FROM - REPLACE_WITH_DIEGO_RESERVED_IP_TO"]
+
+with open(cf_diego_template, 'w') as yaml_file:
+    yaml.dump(cf_diego, yaml_file, Dumper=yaml.SafeDumper, default_flow_style=False)
+
+
